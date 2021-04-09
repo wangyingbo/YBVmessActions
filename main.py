6afd100a9d773a2be0bfe4fb65347228dea31934
Tony <skywolf627@qq.com>
2021-04-09 14:20:13 +0800

    整理

--- main.py
+++ main.py
@@ -6,29 +6,35 @@ import logging
 import traceback
 import os
 import random
-import notify
+# import notify
 import datetime
 import feedparser
+import utils
+import wxpusher
 from lxml.html import fromstring
 import pytz
 
 
 def main(event, context):
-    config = readJson()
+    config = utils.readJsonFile('config.json')
     getFeeds()
-    notify.sendWxPusherByTopic(config["token"], "1832")
-    # for user in users:
-    #     notify.sendPushplus(user["appId"])
+    content = utils.getLogContent()
+    wxpusher.sendTopicMessage(
+        config["token"],
+        '测试推送',
+        content,
+        '1832',
+        'https://fund.lsj8.ltd'
+    )
 
-    # 清空日志记录
-    open('./log.txt', mode='w', encoding='utf-8')
+    utils.clearLog()
+    # # 清空日志记录
+    # open('./log.txt', mode='w', encoding='utf-8')
 
 
 def getFeeds():
     rss = feedparser.parse('http://feeds.feedburner.com/mattkaydiary/pZjG')
     current = rss["entries"][0]
-    # print(rss["entries"][0]["summary"])
-    # logging.info('【发布时间】: ' + current["published"])
     result = re.findall(r"vmess://(.+?)</div>", rss["entries"][0]["summary"])
     i = 0
     for point in result:
@@ -36,35 +42,8 @@ def getFeeds():
         logging.info('【'+('%02d' % i) + '】 vmess://' + point)
 
 
-# 读取用户配置信息
-# 错误原因有两种：格式错误、未读取到错误
-def readJson():
-    try:
-        with open('./config.json', 'r') as fp:
-            config = json.load(fp)
-            return config
-    except Exception as e:
-        print(traceback.format_exc())
-        logging.error('账号信息获取失败错误，原因为: ' + str(e))
-        logging.error('1.请检查是否在Secrets添加了账号信息，以及添加的位置是否正确。')
-        logging.error('2.填写之前，是否在网站验证过Json格式的正确性。')
-
-
 # 主函数入口
 if __name__ == '__main__':
-    # 日志基础配置
-    # 创建一个logger
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    # 创建一个handler，用于写入日志文件
-    # w 模式会记住上次日志记录的位置
-    fh = logging.FileHandler('./log.txt', mode='a', encoding='utf-8')
-    fh.setFormatter(logging.Formatter("%(message)s"))
-    logger.addHandler(fh)
-    # 创建一个handler，输出到控制台
-    ch = logging.StreamHandler()
-    ch.setFormatter(logging.Formatter(
-        "[%(asctime)s]:%(levelname)s:%(message)s"))
-    logger.addHandler(ch)
-
+    # 初始化日志文件
+    utils.initLog('log.txt')
     main("", "")
--- wxpusher.py
+++ wxpusher.py
@@ -1,44 +1,24 @@
-
 # -*- coding: utf-8 -*-
 # @Time    : 2021/4/9 06:30
 # @Author  : skywolf627
 # @Email   : skywolf627@qq.com
 
-import smtplib
 import traceback
 import os
 import requests
 import urllib
 import json
 
-# 发送push+通知
-def sendPushplus(token):
-    try:
-        # 发送内容
-        data = {
-            "token": token,
-            "title": "VMESS每日节点",
-            "content": readFile_html('./log.txt')
-        }
-        url = 'http://www.pushplus.plus/send'
-        headers = {'Content-Type': 'application/json'}
-        body = json.dumps(data).encode(encoding='utf-8')
-        resp = requests.post(url, data=body, headers=headers)
-        print(resp)
-    except Exception as e:
-        print('push+通知推送异常，原因为: ' + str(e))
-        print(traceback.format_exc())
-
-# 发送wxpusher 群消息
-def sendWxPusherByTopic(appToken, topicId):
+# 发送主题消息
+def sendSingleMessage(appToken, title, content, uid, msgUrl):
     try:
         data = {
             "appToken": appToken,
-            "content": readFile_html('./log.txt'),
-            "summary": "最新VMESS节点",
+            "content": content,
+            "summary": title,
             "contentType": 1,
-            "topicIds":  [topicId],
-            "url": "https://fund.lsj8.ltd"
+            "uids":  uid,
+            "url": msgUrl
         }
         url = 'http://wxpusher.zjiecode.com/api/send/message'
         headers = {'Content-Type': 'application/json'}
@@ -50,13 +30,22 @@ def sendWxPusherByTopic(appToken, topicId):
         print(traceback.format_exc())
 
 
-
-# 返回要推送的通知内容
-# 对html的适配要更好
-# 增加文件关闭操作
-def readFile_html(filepath):
-    content = ''
-    with open(filepath, "r", encoding='utf-8') as f:
-        for line in f.readlines():
-            content += line + '<br>'
-    return content
+# 发送单个消息
+def sendTopicMessage(appToken, title, content, topicId, msgUrl):
+    try:
+        data = {
+            "appToken": appToken,
+            "content": content,
+            "summary": title,
+            "contentType": 1,
+            "topicIds":  topicId,
+            "url": msgUrl
+        }
+        url = 'http://wxpusher.zjiecode.com/api/send/message'
+        headers = {'Content-Type': 'application/json'}
+        body = json.dumps(data).encode(encoding='utf-8')
+        resp = requests.post(url, data=body, headers=headers)
+        print(resp)
+    except Exception as e:
+        print('wxpusher通知推送异常，原因为: ' + str(e))
+        print(traceback.format_exc())
